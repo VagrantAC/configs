c2902fa34955afd151dee3321b50034b11414231
VagrantAC <vagrantac@126.com>
2022-12-26 23:09:13 +0800

    add resource page

---
+++ blog/hexo/source/resource/index.md
@@ -0,0 +1,8 @@
+---
+title: 资料 
+layout: resource
+date: 2022-12-25 22:20:57
+---
+
+### 计算机基础 
+- http3 https://http3-explained.haxx.se/
--- blog/shell.nix
+++ blog/shell.nix
@@ -4,6 +4,7 @@ pkgs.mkShell {
   nativeBuildInputs = [
     pkgs.nodejs-16_x
     pkgs.yarn
+    pkgs.pandoc
   ];
   shellHook = ''
     export PATH="$PWD/node_modules/.bin:$PATH";
